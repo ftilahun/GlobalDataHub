81e8f1bde45c2d75fd615e060e38168fb2714798
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-10 17:50:01 +0000

    feature/cdcloader: added a comment

--- source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/Contexts.scala
+++ source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/Contexts.scala
@@ -2,11 +2,10 @@ package com.kainos.enstar.globaldatahub
 
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.hive.HiveContext
 
 /**
- * Created by ciaranke on 10/11/2016.
- */
+  * Singleton for accessing spark/sql context.
+  */
 object Contexts {
   private val _sc : SparkContext = new SparkContext( new SparkConf() )
   private val _sqlC : SQLContext = new SQLContext( _sc )
