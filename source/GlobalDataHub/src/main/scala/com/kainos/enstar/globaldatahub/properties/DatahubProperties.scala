a680085ce6a4c713e90dc6e5daf89c02b9006e6d
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-10 17:48:32 +0000

    feature/cdcloader: properties are now k->v

---
+++ source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/properties/DatahubProperties.scala
@@ -0,0 +1,16 @@
+package com.kainos.enstar.globaldatahub.properties
+
+
+/**
+  * Trait for reading property values.
+  */
+trait DatahubProperties {
+
+  def getBoolenProperty(s: String): Boolean = ???
+
+  def getStringProperty(name : String) : String = ???
+
+  def getArrayProperty(name : String) : Array[String] = ???
+
+  def checkPropertiesSet
+}
--- source/GlobalDataHub/src/test/scala/com/kainos/enstar/globaldatahub/cdcloader/control/ControlProcessorSpec.scala
+++ source/GlobalDataHub/src/test/scala/com/kainos/enstar/globaldatahub/cdcloader/control/ControlProcessorSpec.scala
@@ -7,8 +7,11 @@ import org.apache.spark.sql.DataFrame
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.{DateTime, DateTimeUtils}
 import org.mockito.Mockito
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, GivenWhenThen, Matchers}
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
 
+/**
+  * Unit tests for the control processor
+  */
 class ControlProcessorSpec extends FlatSpec
     with GivenWhenThen
     with Matchers {
@@ -21,25 +24,25 @@ class ControlProcessorSpec extends FlatSpec
       new ControlProcessor( TestContexts.sqlContext, loaderIO, sqlReaderIO, properties )
 
     Given( "The input /control/dir/" )
-    Mockito.when( properties.controlTablePath ).thenReturn( "/control/dir/" )
-    Mockito.when( properties.controlTableName ).thenReturn( "control" )
+    Mockito.when( properties.getStringProperty("controlTablePath") ).thenReturn( "/control/dir/" )
+    Mockito.when( properties.getStringProperty("controlTableName") ).thenReturn( "control" )
     When( "The input is valid" )
-    Mockito.when( loaderIO.read( TestContexts.sqlContext, properties.controlTablePath, None ) )
+    Mockito.when( loaderIO.read( TestContexts.sqlContext, properties.getStringProperty("controlTablePath"), None ) )
       .thenReturn( TestContexts.generateControlTable( 10 ) )
     Mockito.when( loaderIO.registerTempTable( org.mockito.Matchers.any( classOf[DataFrame] ),
       org.mockito.Matchers.anyString() ) ).thenCallRealMethod()
     Then( "The control table should be created" )
     controlProcessor.registerControlTable
-    TestContexts.sqlContext.sql( "SELECT * FROM " + properties.controlTableName ).count should be ( 10 )
+    TestContexts.sqlContext.sql( "SELECT * FROM " + properties.getStringProperty("controlTableName") ).count should be ( 10 )
 
     Given( "The control table has been created" )
     When( "The control table is de-registered" )
-    Mockito.when( loaderIO.deRegisterTempTable( TestContexts.sqlContext, properties.controlTableName ) ).
+    Mockito.when( loaderIO.deRegisterTempTable( TestContexts.sqlContext, properties.getStringProperty("controlTableName") ) ).
       thenCallRealMethod()
     controlProcessor.deregisterControlTable
     Then( "an exception should be thrown" )
     an[RuntimeException] should be thrownBy {
-      TestContexts.sqlContext.sql( "SELECT * FROM " + properties.controlTableName )
+      TestContexts.sqlContext.sql( "SELECT * FROM " + properties.getStringProperty("controlTableName") )
     }
   }
 
@@ -54,21 +57,21 @@ class ControlProcessorSpec extends FlatSpec
     DateTimeUtils.setCurrentMillisFixed( date.getMillis )
 
     Given("A populated control table")
-    Mockito.when( properties.controlTablePath ).thenReturn( "/control/dir/" )
-    Mockito.when( properties.controlTableName ).thenReturn( "control" )
-    Mockito.when(properties.changeSequenceTimestampFormat).thenReturn("YYYYMMDDHHmmSShh")
-    Mockito.when( loaderIO.read( TestContexts.sqlContext, properties.controlTablePath, None ) )
+    Mockito.when( properties.getStringProperty("controlTablePath")).thenReturn( "/control/dir/" )
+    Mockito.when( properties.getStringProperty("controlTableName") ).thenReturn( "control" )
+    Mockito.when(properties.getStringProperty("changeSequenceTimestampFormat")).thenReturn("YYYYMMDDHHmmSShh")
+    Mockito.when( loaderIO.read( TestContexts.sqlContext, properties.getStringProperty("controlTablePath"), None ) )
       .thenReturn( TestContexts.generateControlTable( 10 ) )
     Mockito.when( loaderIO.registerTempTable( org.mockito.Matchers.any( classOf[DataFrame] ),
       org.mockito.Matchers.anyString() ) ).thenCallRealMethod()
     controlProcessor.registerControlTable
     When("The control table has 10 rows")
-    Mockito.when(properties.controlTableSQLPath).thenReturn("/some/path")
-    Mockito.when(sqlReaderIO.getSQLString( TestContexts.sparkContext, properties.controlTableSQLPath ))
+    Mockito.when(properties.getStringProperty("controlTableSQLPath")).thenReturn("/some/path")
+    Mockito.when(sqlReaderIO.getSQLString( TestContexts.sparkContext, properties.getStringProperty("controlTableSQLPath") ))
       .thenReturn("SELECT MAX(lastattunitychangeseq) FROM control WHERE attunitytablename = ")
     Then("The ControlProcessor should return the seqence number of the 10th row")
     controlProcessor.getLastSequenceNumber("'policy'") should be (
-      DateTimeFormat.forPattern( properties.changeSequenceTimestampFormat ).print( date ) +
+      DateTimeFormat.forPattern( properties.getStringProperty("changeSequenceTimestampFormat") ).print( date ) +
       "0000000000000000010"
     )
     DateTimeUtils.setCurrentMillisSystem()
@@ -86,9 +89,9 @@ class ControlProcessorSpec extends FlatSpec
     DateTimeUtils.setCurrentMillisFixed( date.getMillis )
     When("Required")
     Then("The control processor should generate a sequence number")
-    Mockito.when(properties.changeSequenceTimestampFormat).thenReturn("YYYYMMDDHHmmSShh")
+    Mockito.when(properties.getStringProperty("changeSequenceTimestampFormat")).thenReturn("YYYYMMDDHHmmSShh")
     controlProcessor.generateFirstSequenceNumber should be (
-      DateTimeFormat.forPattern( properties.changeSequenceTimestampFormat ).print( date ) +
+      DateTimeFormat.forPattern( properties.getStringProperty("changeSequenceTimestampFormat") ).print( date ) +
       "0000000000000000000"
     )
     DateTimeUtils.setCurrentMillisSystem()
