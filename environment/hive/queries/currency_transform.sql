2e15d907078bd9fa46df1614e90422d61ff29b2d
caoimheb <c.boyle@kainos.com>
2016-11-15 17:07:53 +0000

    feature/CurrencyMapping: Added static mapping

---
+++ environment/hive/queries/currency_transform.sql
@@ -0,0 +1,13 @@
+SELECT
+
+currency_code AS currencycode,
+'NDEX' AS sourcesystemcode,
+currency_desc AS description,
+_isdeleted,
+_changeoperation,
+_loaddatetimestamp,
+_loadidentifier
+
+FROM
+
+lookup_currency
