dfd00f0b64cfe75dc14859e8d19f8623116bb013
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-20 15:38:01 +0000

    feature/cdcloader: added instanciator object

---
+++ source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/cdcloader/instanciator/CDCLoaderInstanciator.scala
@@ -0,0 +1,107 @@
+package com.kainos.enstar.globaldatahub.cdcloader.instanciator
+
+import com.kainos.enstar.globaldatahub.cdcloader.control.{
+  CDCControlProcessor,
+  ControlProcessor
+}
+import com.kainos.enstar.globaldatahub.cdcloader.io._
+import com.kainos.enstar.globaldatahub.cdcloader.processor.{
+  CDCSourceProcessor,
+  CDCTableProcessor,
+  SourceProcessor,
+  TableProcessor
+}
+import com.kainos.enstar.globaldatahub.cdcloader.properties.CDCProperties
+import com.kainos.enstar.globaldatahub.cdcloader.udfs.{
+  CDCUserFunctions,
+  UserFunctions
+}
+import com.kainos.enstar.globaldatahub.io.{
+  AvroDataFrameReader,
+  AvroDataFrameWriter,
+  TextFileReader
+}
+import com.kainos.enstar.globaldatahub.properties.GDHProperties
+
+/**
+ * Creates required objects for this job.
+ */
+object CDCLoaderInstanciator {
+
+  private val _controlProcessor : ControlProcessor = new CDCControlProcessor
+  private val _cdcSourceProcessor : SourceProcessor = new CDCSourceProcessor
+  private val _dataFrameReader : DataFrameReader = new CDCDataFrameReader(
+    new AvroDataFrameReader )
+  private val _dataFrameWriter : DataFrameWriter = new CDCDataFrameWriter(
+    new AvroDataFrameWriter )
+  private val _tableOperations : TableOperations = new CDCTableOperations
+  private val _tableProcessor : TableProcessor = new CDCTableProcessor
+  private val _userFunctions : UserFunctions = new CDCUserFunctions
+  private val _sqlReader : SQLFileReader = new CDCSQLFileReader(
+    new TextFileReader )
+
+  /**
+   * Get the ControlProcessor
+   *
+   * @return a ControlProcessor
+   */
+  def controlProcessor : ControlProcessor = _controlProcessor
+
+  /**
+   * Get the SourceProcessor
+   *
+   * @return a SourceProcessor
+   */
+  def cdcSourceProcessor : SourceProcessor = _cdcSourceProcessor
+
+  /**
+   * Get the DataFrameReader
+   *
+   * @return a DataFrameReader
+   */
+  def dataFrameReader : DataFrameReader = _dataFrameReader
+
+  /**
+   * Get the DataFrameWriter
+   *
+   * @return a DataFrameWriter
+   */
+  def dataFrameWriter : DataFrameWriter = _dataFrameWriter
+
+  /**
+   * Get the TableOperations
+   *
+   * @return a TableOperations object
+   */
+  def tableOperations : TableOperations = _tableOperations
+
+  /**
+   * Get the TableProcessor
+   *
+   * @return a TableProcessor
+   */
+  def tableProcessor : TableProcessor = _tableProcessor
+
+  /**
+   * Get a UserFunctions object
+   *
+   * @return a UserFunctions object
+   */
+  def userFunctions : UserFunctions = _userFunctions
+
+  /**
+   * Get an SQLFileReader
+   *
+   * @return an SQLFileReader
+   */
+  def sqlReader : SQLFileReader = _sqlReader
+
+  /**
+   * Get the properties object
+   * @param args The command line arguments
+   * @return a properties object
+   */
+  def properties( args : Array[String] ) : GDHProperties =
+    new CDCProperties( CDCProperties.parseProperties( args ) )
+
+}
---
+++ source/GlobalDataHub/src/test/scala/com/kainos/enstar/globaldatahub/cdcloader/instanciator/CDCLoaderInstanciatorSpec.scala
@@ -0,0 +1,86 @@
+package com.kainos.enstar.globaldatahub.cdcloader.instanciator
+
+import com.kainos.enstar.globaldatahub.cdcloader.control.ControlProcessor
+import com.kainos.enstar.globaldatahub.cdcloader.io.{
+  DataFrameReader,
+  DataFrameWriter,
+  SQLFileReader,
+  TableOperations
+}
+import com.kainos.enstar.globaldatahub.cdcloader.processor.{
+  SourceProcessor,
+  TableProcessor
+}
+import com.kainos.enstar.globaldatahub.cdcloader.udfs.UserFunctions
+import com.kainos.enstar.globaldatahub.properties.GDHProperties
+import org.apache.spark.sql.SQLContext
+import org.mockito.Mockito
+import org.scalatest.{ FlatSpec, GivenWhenThen, Matchers }
+
+/**
+ * Unit tests for CDCLoaderInstanciator
+ */
+class CDCLoaderInstanciatorSpec
+    extends FlatSpec
+    with GivenWhenThen
+    with Matchers {
+
+  "CDCLoaderInstanciatorSpec" should "Return correct tyoes" in {
+      def typeCheck[T]( value : T ) = value match {
+        case _ : ControlProcessor => "ControlProcessor"
+        case _ : SourceProcessor  => "SourceProcessor"
+        case _ : DataFrameReader  => "DataFrameReader"
+        case _ : DataFrameWriter  => "DataFrameWriter"
+        case _ : TableOperations  => "TableOperations"
+        case _ : TableProcessor   => "TableProcessor"
+        case _ : UserFunctions    => "UserFunctions"
+        case _ : SQLFileReader    => "SQLFileReader"
+        case _ : GDHProperties    => "GDHProperties"
+      }
+
+    val args = Array[String](
+      "--cdcOptions",
+      "spark.cdcloader.columns.attunity.name.changemask=a," +
+        "spark.cdcloader.columns.control.names.controlcolumnnames=a," +
+        "spark.cdcloader.columns.attunity.name.changeoperation=a," +
+        "spark.cdcloader.columns.attunity.name.changesequence=a," +
+        "spark.cdcloader.columns.attunity.value.changeoperation=a," +
+        "spark.cdcloader.columns.metadata.name.isdeleted=true," +
+        "spark.cdcloader.control.attunity.changetablesuffix=a," +
+        "spark.cdcloader.columns.metadata.name.loadtimestamp=a," +
+        "spark.cdcloader.paths.data.outputbasedir=a," +
+        "spark.cdcloader.paths.data.outdir=a," +
+        "spark.cdcloader.format.timestamp.attunity=a," +
+        "spark.cdcloader.format.timestamp.hive=a," +
+        "spark.cdcloader.paths.data.basedir=a," +
+        "spark.cdcloader.paths.data.control=a," +
+        "spark.cdcloader.paths.data.output=a," +
+        "spark.cdcloader.paths.sql.basedir=a," +
+        "spark.cdcloader.paths.sql.control=a," +
+        "spark.cdcloader.tables.control.name=a," +
+        "spark.cdcloader.control.changemask.enabled=a," +
+        "spark.cdcloader.input.tablenames=a_b," +
+        "spark.cdcloader.control.columnpositions.a=1_2_3_4," +
+        "spark.cdcloader.columns.control.name.tablename.a=a," +
+        "spark.cdcloader.control.columnpositions.b=1_2_3," +
+        "spark.cdcloader.columns.control.name.tablename.b=b"
+    )
+
+    typeCheck( CDCLoaderInstanciator.controlProcessor ) should be(
+      "ControlProcessor" )
+    typeCheck( CDCLoaderInstanciator.cdcSourceProcessor ) should be(
+      "SourceProcessor" )
+    typeCheck( CDCLoaderInstanciator.dataFrameReader ) should be(
+      "DataFrameReader" )
+    typeCheck( CDCLoaderInstanciator.dataFrameWriter ) should be(
+      "DataFrameWriter" )
+    typeCheck( CDCLoaderInstanciator.tableOperations ) should be(
+      "TableOperations" )
+    typeCheck( CDCLoaderInstanciator.tableProcessor ) should be( "TableProcessor" )
+    typeCheck( CDCLoaderInstanciator.userFunctions ) should be( "UserFunctions" )
+    typeCheck( CDCLoaderInstanciator.sqlReader ) should be( "SQLFileReader" )
+    typeCheck( CDCLoaderInstanciator.properties( args ) ) should be(
+      "GDHProperties" )
+  }
+
+}
