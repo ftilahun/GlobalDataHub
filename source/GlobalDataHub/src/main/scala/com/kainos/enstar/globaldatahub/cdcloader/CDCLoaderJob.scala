da85ac91507be9f68a953ddc28e483d8feacbe41
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-20 15:38:16 +0000

    feature/cdcloader: added main job

--- source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/cdcloader/CDCLoaderJob.scala
+++ source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/cdcloader/CDCLoaderJob.scala
@@ -1,11 +1,29 @@
 package com.kainos.enstar.globaldatahub.cdcloader
 
-import org.apache.spark.Logging
+import com.kainos.enstar.globaldatahub.cdcloader.instanciator.CDCLoaderInstanciator
+import com.kainos.enstar.globaldatahub.cdcloader.processor.CDCSourceProcessor
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{ Logging, SparkConf, SparkContext }
 
 /**
  * Created by ciaranke on 10/11/2016.
  */
 object CDCLoaderJob extends Logging {
 
-  def main( args : Array[String] ) : Unit = {}
+  def main( args : Array[String] ) : Unit = {
+
+    val sparkConf = new SparkConf()
+    val sparkContext : SparkContext = new SparkContext( sparkConf )
+    val sqlContext : SQLContext = new SQLContext( sparkContext )
+
+    new CDCSourceProcessor().process( CDCLoaderInstanciator.controlProcessor,
+      CDCLoaderInstanciator.properties( args ),
+      sqlContext,
+      CDCLoaderInstanciator.dataFrameReader,
+      CDCLoaderInstanciator.dataFrameWriter,
+      CDCLoaderInstanciator.tableOperations,
+      CDCLoaderInstanciator.tableProcessor,
+      CDCLoaderInstanciator.userFunctions,
+      CDCLoaderInstanciator.sqlReader )
+  }
 }
