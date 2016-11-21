fff6d5279fc60bc855deb4eb933a1f9c51666db6
caoimheb <c.boyle@kainos.com>
2016-11-21 16:28:09 +0000

    feature/PolicyTestData: WIP

---
+++ environment/hive/queries/policy_transform.sql
@@ -0,0 +1,29 @@
+SELECT
+
+AS policynumber, 
+AS coveragereference, 
+AS sectionreference,
+AS sourcesystemcode, 
+AS branchcode, 
+AS branchdescription,
+AS cancellationdate,
+AS coverageeffectivefromdate, 
+AS coverageeffectivetodate,
+AS expirydate,
+AS filcode, 
+AS inceptiondate,
+AS legacypolicynumber,
+AS legalentitycode, 
+AS lineofbusinesscode,
+AS lineofbusinessdescription,
+AS linkedmasterreference, 
+AS majorriskcode, 
+AS majortrustfundcode, 
+AS sourcesystempolicynumber, 
+AS sublineofbusinesscode, 
+AS sublineofbusinessdescription, 
+AS uniquemarketreference, 
+AS yearofaccount
+
+FROM
+
