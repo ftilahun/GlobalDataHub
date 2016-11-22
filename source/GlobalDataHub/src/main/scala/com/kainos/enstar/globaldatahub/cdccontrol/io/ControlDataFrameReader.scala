fab0f81b197568d1f0679ff1c88ed2703acc943f
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-22 12:03:12 +0000

    featurte/cdccontrolJob: added reader class

---
+++ source/GlobalDataHub/src/main/scala/com/kainos/enstar/globaldatahub/cdccontrol/io/ControlDataFrameReader.scala
@@ -0,0 +1,29 @@
+package com.kainos.enstar.globaldatahub.cdccontrol.io
+
+import com.kainos.enstar.globaldatahub.common.io.DataFrameReader
+import org.apache.spark.Logging
+import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.apache.spark.storage.StorageLevel
+
+/**
+ * Created by ciaranke on 22/11/2016.
+ */
+class ControlDataFrameReader( reader : DataFrameReader )
+    extends DataFrameReader
+    with Logging {
+
+  /**
+   * read a dataframe from hdfs
+   *
+   * @param sqlContext   the hive context
+   * @param path         the path to read from
+   * @param storageLevel an optional storagelevel to persist the dataframe
+   * @return a dataframe
+   */
+  override def read( sqlContext : SQLContext,
+                     path : String,
+                     storageLevel : Option[StorageLevel] ) : DataFrame = {
+    reader.read( sqlContext, path, storageLevel )
+  }
+
+}
