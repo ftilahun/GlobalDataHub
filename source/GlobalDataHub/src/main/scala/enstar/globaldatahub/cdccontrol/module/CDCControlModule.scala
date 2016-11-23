38f2b9236182bbf33708be677cf37a8428d5f860
Ciaran Kearney <C.Kearney@kainos.com>
2016-11-23 16:50:04 +0000

    feature/cdccontrolJob: added Control module

---
+++ source/GlobalDataHub/src/main/scala/enstar/globaldatahub/cdccontrol/module/CDCControlModule.scala
@@ -0,0 +1,58 @@
+package enstar.globaldatahub.cdccontrol.module
+
+import enstar.globaldatahub.cdccontrol.io.{ ControlDataFrameReader, ControlDataFrameWriter }
+import enstar.globaldatahub.cdccontrol.properties.ControlProperties
+import enstar.globaldatahub.common.io._
+import enstar.globaldatahub.common.properties.GDHProperties
+import org.apache.spark.Logging
+
+/**
+ * Creates required objects for this job.
+ */
+object CDCControlModule extends Logging {
+
+  logInfo( "Creating required objects" )
+  private val _dataFrameReader : DataFrameReader = new ControlDataFrameReader(
+    new AvroDataFrameReader )
+  private val _dataFrameWriter : DataFrameWriter = new ControlDataFrameWriter(
+    new AvroDataFrameWriter )
+  private val _tableOperations : TableOperations = new DataFrameTableOperations
+  private val _sqlReader : SQLReader = new SQLFileReader( new TextFileReader )
+  logInfo( "Completed required object creation" )
+
+  /**
+   * Get the properties object
+   * @param propertyMap a Map of k->v objects
+   * @return a properties object
+   */
+  def properties( propertyMap : Map[String, String] ) : GDHProperties =
+    new ControlProperties( propertyMap )
+
+  /**
+   * Get the TableOperations
+   *
+   * @return a TableOperations object
+   */
+  def tableOperations : TableOperations = _tableOperations
+  /**
+   * Get an SQLFileReader
+   *
+   * @return an SQLFileReader
+   */
+  def sqlReader : SQLReader = _sqlReader
+
+  /**
+   * Get the DataFrameWriter
+   *
+   * @return a DataFrameWriter
+   */
+  def dataFrameWriter : DataFrameWriter = _dataFrameWriter
+
+  /**
+   * Get the DataFrameReader
+   *
+   * @return a DataFrameReader
+   */
+  def dataFrameReader : DataFrameReader = _dataFrameReader
+
+}
---
+++ source/GlobalDataHub/src/test/scala/enstar/globaldatahub/cdccontrol/module/CDCControlModuleSpec.scala
@@ -0,0 +1,40 @@
+package enstar.globaldatahub.cdccontrol.module
+
+import enstar.globaldatahub.cdccontrol.properties.ControlProperties
+import enstar.globaldatahub.common.io.{DataFrameReader, DataFrameWriter, SQLReader, TableOperations}
+import enstar.globaldatahub.common.properties.GDHProperties
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
+
+/**
+  * Unit tests for CDCControlModule
+  */
+class CDCControlModuleSpec
+  extends FlatSpec
+    with GivenWhenThen
+    with Matchers {
+  "CDCControlModule" should "Return correct types" in {
+    def typeCheck[T]( value : T ) = value match {
+      case _ : DataFrameReader  => "DataFrameReader"
+      case _ : DataFrameWriter  => "DataFrameWriter"
+      case _ : TableOperations  => "TableOperations"
+      case _ : SQLReader        => "SQLFileReader"
+      case _ : GDHProperties    => "GDHProperties"
+    }
+
+    val args = ControlProperties.parseProperties( Array[String](
+      "--ctrlOptions",
+      "spark.cdccontrol.path.sql=a," +
+        "spark.cdccontrol.path.data.control.input=b," +
+        "spark.cdccontrol.path.data.control.output=c," +
+        "spark.cdccontrol.path.data.input=d," +
+        "spark.cdccontrol.tables.control.name=e," +
+        "spark.cdccontrol.tables.temp.name=f"
+    ) )
+
+    typeCheck( CDCControlModule.dataFrameReader ) should be( "DataFrameReader" )
+    typeCheck( CDCControlModule.dataFrameWriter ) should be( "DataFrameWriter" )
+    typeCheck( CDCControlModule.tableOperations ) should be( "TableOperations" )
+    typeCheck( CDCControlModule.sqlReader ) should be( "SQLFileReader" )
+    typeCheck( CDCControlModule.properties( args ) ) should be( "GDHProperties" )
+  }
+}
