e45bb60426a89cc5a7355b6be5356756e0eeb896
tsheppard01 <t.sheppard@kainos.com>
2016-11-29 14:03:53 +0000

    Policy_mapping: Post merge fixes.

--- source/TransformationTesting/src/main/scala/com/kainos/enstar/TransformationUnitTesting/TransactionTypeUtils.scala
+++ source/TransformationTesting/src/main/scala/com/kainos/enstar/TransformationUnitTesting/TransactionTypeUtils.scala
@@ -4,12 +4,12 @@ import org.apache.spark.sql.Row
 
 object TransactionTypeUtils {
 
-  def lookupPremiumTypeMapping(cols : Array[String]): Row = {
-    Row(cols(0), cols(1))
+  def lookupPremiumTypeMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ) )
   }
 
-  def transactionTypeMapping(cols : Array[String]): Row = {
-    Row(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6).toBoolean)
+  def transactionTypeMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ), cols( 2 ), cols( 3 ), cols( 4 ), cols( 5 ), cols( 6 ).toBoolean )
   }
 
 }
