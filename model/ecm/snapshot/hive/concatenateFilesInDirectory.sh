02c9cab7bfbc2a60a2d0c1c635d3f89fa283e5b4
dmc1899 <darragh@kainos.com>
2016-12-01 11:12:19 +0000

    Adding file concatenation script.

--- model/ecm/snapshot/hive/concatenateFilesInDirectory.sh
+++ model/ecm/snapshot/hive/concatenateFilesInDirectory.sh
@@ -1,25 +1,29 @@
 #set -x
 #!/bin/sh
 #
-# Combines the contents of all files in a single directory together.
+# Combines the contents of all files from a single directory into a single file.
+# The use case for this utility is combining all SQL files into a single SQL script to run with the Beeline CLI.
 #
 # Expects two parameters:
 #	1: Directory name containing the source files.
 #	2: Target filename including path to create the composite file.
 #
-
-if [ $# == 0 ] ; then
-    echo "Usage: $0 source_directory destination_file_path"
+if [ ${#} -ne 2 ] || ! [ -d ${1} ]; then
+    echo "Usage: $0 source_directory destination_file_name"
     exit 1;
 fi
 
 logFile="$0.log"
 sourceDirectory=$1
-targetFilePath=$2
+targetFileName=$2
+scriptDirectory=`pwd`
 
 cd ${sourceDirectory}
-for file in `ls` 
+for file in `ls -p | grep -v /` 
 do
-	cat "$file" >> ${targetFilePath}
+	echo ${file}
+	cat ${file} >> ${scriptDirectory}/${targetFileName}
 done
+cd ${scriptDirectory}
+
 exit
