faa94f5349f93c23fb1edd901543d40281f4e08e
Siona Murray <sionam@suva.local>
2016-12-02 14:34:21 +0000

    feature/policytransactiondeductions_mapping: Adding Utils class and removing header row from test input files.

---
+++ source/TransformationTesting/src/main/scala/com/kainos/enstar/TransformationUnitTesting/PolicyTransactionDeductionsUtils.scala
@@ -0,0 +1,76 @@
+package com.kainos.enstar.TransformationUnitTesting
+
+import org.apache.spark.sql.Row
+
+/**
+ * Created by sionam on 02/12/2016.
+ */
+object PolicyTransactionDeductionsUtils {
+
+  def lineMapping( cols : Array[String] ) : Row = cols match {
+    case cols if cols.length == 4 => Row( cols( 0 ), cols( 1 ), cols( 2 ), cols( 3 ), null )
+    case cols if cols.length == 3 => Row( cols( 0 ), cols( 1 ), cols( 2 ), null, null )
+    case cols if cols.length == 2 => Row( cols( 0 ), cols( 1 ), null, null, null )
+    case _                        => Row( cols( 0 ), cols( 1 ), cols( 2 ), cols( 3 ), cols( 4 ) )
+  }
+
+  def layerMapping( cols : Array[String] ) : Row = cols match {
+      case cols if cols.length == 2 => Row( cols( 0 ), cols( 1 ), null )
+      case _                        => Row( cols( 0 ), cols( 1 ), cols( 2 ) )
+  }
+
+  def layerDeductionMapping( cols : Array[String] ) : Row = {
+    cols match {
+      case cols if cols.length == 2 => Row( cols( 0 ), cols( 1 ), null )
+      case _                        => Row( cols( 0 ), cols( 1 ), cols( 2 ) )
+    }  }
+
+  def layerTrustFundMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ) )
+  }
+
+  def lineRiskCodeMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ) )
+  }
+
+  def lookupDeductionTypeMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ) )
+  }
+
+  def riskCodeMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ))
+  }
+
+  def settlementScheduleMapping( cols : Array[String] ) : Row =  cols match {
+    case cols if cols.length == 4 => Row( cols( 0 ), cols( 1 ), cols( 2 ), cols( 3 ), null )
+    case cols if cols.length == 3 => Row( cols( 0 ), cols( 1 ), cols( 2 ), null, null )
+    case cols if cols.length == 2 => Row( cols( 0 ), cols( 1 ), null, null, null )
+    case _                        => Row( cols( 0 ), cols( 1 ), cols( 2 ), cols( 3 ), cols( 4 ) )
+  }
+
+  def trustFundIndicatorMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ))
+  }
+
+  def policyTransactionDeductionsMapping( cols : Array[String] ) : Row = {
+    Row( cols( 0 ), cols( 1 ),
+      if ( cols( 2 ).equals( "" ) ) null else cols( 2 ),
+      if ( cols( 3 ).equals( "" ) ) null else cols( 3 ),
+      if ( cols( 4 ).equals( "" ) ) null else cols( 4 ),
+      if ( cols( 5 ).equals( "" ) ) null else cols( 5 ),
+      if ( cols( 6 ).equals( "" ) ) null else cols( 6 ),
+      if ( cols( 7 ).equals( "" ) ) null else cols( 7 ),
+      if ( cols( 8 ).equals( "" ) ) null else cols( 8 ),
+      if ( cols( 9 ).equals( "" ) ) null else cols( 9 ),
+      if ( cols( 10 ).equals( "" ) ) null else cols( 10 ),
+      if ( cols( 11 ).equals( "" ) ) null else cols( 11 ),
+      if ( cols( 12 ).equals( "" ) ) null else cols( 12 ),
+      if ( cols( 13 ).equals( "" ) ) null else cols( 13 ),
+      if ( cols( 14 ).equals( "" ) ) null else cols( 14 ),
+      if ( cols( 15 ).equals( "" ) ) null else cols( 15 ),
+      if ( cols( 16 ).equals( "" ) ) null else cols( 16 ),
+      if ( cols( 17 ).equals( "" ) ) null else cols( 17 )
+    )
+  }
+
+}
