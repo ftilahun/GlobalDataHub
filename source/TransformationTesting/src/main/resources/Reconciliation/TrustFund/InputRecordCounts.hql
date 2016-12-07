e8dd0c95d4d38eec850e40ed8bba86387438f64b
caoimheb <c.boyle@kainos.com>
2016-12-07 15:49:57 +0000

    feature/trustfund_mapping: Added test data, schemas, transform and tests for TrustFund

---
+++ source/TransformationTesting/src/main/resources/Reconciliation/TrustFund/InputRecordCounts.hql
@@ -0,0 +1,3 @@
+SELECT
+    COUNT(*) AS recordcount
+FROM lookup_trust_fund
---
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/TrustFund/ReconciliationTests.scala
@@ -0,0 +1,50 @@
+package com.kainos.enstar.test.TransformationUnitTesting.TrustFund
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import com.kainos.enstar.TransformationUnitTesting.{ SQLRunner, TransformationUnitTestingUtils, TrustFundUtils }
+import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.scalatest.FunSuite
+
+/**
+ * Created by caoimheb on 07/12/2016.
+ */
+class ReconciliationTests extends FunSuite with DataFrameSuiteBase {
+
+  private val utils = new TransformationUnitTestingUtils
+
+  def populateDataFrameWithLookupTrustFundTestData( dataFileName : String, sqlc : SQLContext ) : DataFrame = {
+
+    utils.populateDataFrameFromFile(
+      getClass.getResource( "/trustfund/input/" + dataFileName ).toString,
+      getClass.getResource( "/trustfund/schemas/lookup_trust_fund.avro" ).toString,
+      _.split( "," ),
+      TrustFundUtils.lookupTrustFundMapping,
+      sqlc
+    )
+  }
+
+  test( "TrustFund reconciliation over test data" ) {
+
+    // Arrange //
+    val sqlc = sqlContext
+    val utils = new TransformationUnitTestingUtils
+
+    val lookup_trust_fund = this.populateDataFrameWithLookupTrustFundTestData( "lookup_trust_fund_test1.csv", sqlc )
+
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/TrustFund.hql" )
+    val reconStatementInput = utils.loadHQLStatementFromResource( "Reconciliation/TrustFund/InputRecordCounts.hql" )
+    val reconStatementOutput = utils.loadHQLStatementFromResource( "Reconciliation/TrustFund/OutputRecordCounts.hql" )
+
+    // Act //
+    lookup_trust_fund.registerTempTable( "lookup_trust_fund" )
+
+    val output = SQLRunner.runStatement( hqlStatement, sqlc )
+    output.registerTempTable( "trustfund" )
+
+    val reconInput = SQLRunner.runStatement( reconStatementInput, sqlc )
+    val reconOutput = SQLRunner.runStatement( reconStatementOutput, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( reconInput, reconOutput )
+  }
+}
