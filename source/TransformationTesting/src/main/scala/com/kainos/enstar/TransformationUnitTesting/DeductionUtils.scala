f2feed49945c6cfe58b86e434a8fc2dafca02b17
tsheppard01 <t.sheppard@kainos.com>
2016-12-15 10:17:12 +0000

    Deduction mapping: added new tests and removed some spike code.

--- source/TransformationTesting/src/main/scala/com/kainos/enstar/TransformationUnitTesting/DeductionUtils.scala
+++ source/TransformationTesting/src/main/scala/com/kainos/enstar/TransformationUnitTesting/DeductionUtils.scala
@@ -53,37 +53,4 @@ object DeductionUtils {
       cols( 11 ).toBoolean
     )
   }
-
-  def getStuff (list: List[Row]): List[Row] = {
-
-    var newList = ListBuffer[Row]()
-
-    for( item <- list) {
-
-      val currentIndex = list.indexOf(item)
-      val currentSequenceNo = item.get(item.fieldIndex("sequence_no"))
-      val currentDeductionId = item.get(item.fieldIndex("deduction_id"))
-      val currentLineId = item.get(item.fieldIndex("line_id"))
-
-      val previousItem = if (currentIndex == 0) null else list(currentIndex - 1)
-      val previousNewItem = if (currentIndex == 0) null else newList(currentIndex - 1)
-      val lastSequenceNo = if (currentIndex == 0) 0 else previousItem.get(previousItem.fieldIndex("sequence_no"))
-      val lastNetAmount =
-        if (currentIndex==0) item(item.fieldIndex("slip_income_amount")).toString.toDouble * (item(item.fieldIndex("reporting_line_pct")).toString.toDouble/100)
-      else previousNewItem.get(3)
-      val lastDeductionAmount = if (currentIndex==0) 0.0 else previousNewItem.get(4)
-
-      val ded = item.get(item.fieldIndex("deduction_pct")).toString.toDouble
-
-      val currentNet =  lastNetAmount.toString.toDouble - lastDeductionAmount.toString.toDouble
-      val deductionAmount =
-        if (currentSequenceNo == lastSequenceNo) lastDeductionAmount
-        else currentNet.toString.toDouble * item.get(item.fieldIndex("deduction_pct")).toString.toDouble / 100
-
-      //create new row and add to the list
-      newList += Row(currentLineId,currentDeductionId,currentSequenceNo,currentNet.toString,deductionAmount.toString)
-    }
-
-    newList.toList
-  }
 }
--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Deduction/TransformationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Deduction/TransformationTests.scala
@@ -107,109 +107,68 @@ class TransformationTests extends FunSuite with DataFrameSuiteBase {
     layerDeduction.registerTempTable( "layer_deduction" )
 
     // Load the hql statement under test
-    val tmptableStatement = utils.loadHQLStatementFromResource("Transformation/DeductionPre.hql")
     val statement = utils.loadHQLStatementFromResource( "Transformation/Deduction.hql" )
 
     // Act
-    val tmptable = SQLRunner.runStatement(tmptableStatement, sqlc).registerTempTable("tmptable")
     val result = SQLRunner.runStatement( statement, sqlc )
 
     // Assert
     assertDataFrameEquals( expectedDeduction, result )
   }
 
-  def add100(column: Column): Column = {
-    column + 100
-  }
+  test("Deduction transformation mapping test multiple deductions on a line, monotonic sequence no") {
 
-  test("testing scala stuffs") {
     // Arrange
     val sqlc = sqlContext
     sqlContext.sparkContext.setLogLevel( "WARN" )
 
     // Load test data into dataframes
-    val line = populateDataFrameWithLineTestData( "line_PrimaryTestData.csv", sqlc )
-    val layer = populateDataFrameWithLayerTestData( "layer_PrimaryTestData.csv", sqlc )
+    val line = populateDataFrameWithLineTestData( "line_MultipleDeductionMonotonicSeq.csv", sqlc )
+    val layer = populateDataFrameWithLayerTestData( "layer_MultipleDeductionMonotonicSeq.csv", sqlc )
     val layerDeduction = populateDataFrameWithLayerDeductionTestData( "layer_deduction_MultipleDeductionMonotonicSeq.csv", sqlc )
 
-    sqlc.udf.register("add100",add100 : Int => Int);
+    val expectedDeduction = populateDataFrameWithDeductionTestData( "deduction_MultipleDeductionMonotonicSeq.csv", sqlc )
 
     line.registerTempTable( "line" )
     layer.registerTempTable( "layer" )
     layerDeduction.registerTempTable( "layer_deduction" )
 
-    val win = Window.partitionBy("line_id").orderBy("sequence_no")
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( "Transformation/Deduction.hql" )
 
-    val layerDeductionJoinedWithLine = layerDeduction.join(line, "layer_id")
-    val allFirstDeductions =
-      layerDeductionJoinedWithLine
-      .withColumn("gross", col("slip_income_amount") * (col("reporting_line_pct")/100))
-      .withColumn("lastSequence", lag("sequence_no",1) over win)
-      .withColumn("calculateddeductionamount",
-        when(col("sequence_no") > 1, null)
-          otherwise col("gross") - col("deduction_pct")/100
-      )
-      .withColumn("currentnet",
-        when(col("sequence_no") > 1, null)
-           otherwise col("gross") - col("calculatedDeductionAmount")
-      )
-          .withColumn("netforthissequenceno", col("gross"))
-        .collect()
-
-    //SQLRunner.runStatement("Select sequence_no + 100 as d from layer_deduction", sqlc).collect().foreach(println)
-    val statement = utils.loadHQLStatementFromResource( "Transformation/DeductionPre2.hql" )
-
+    // Act
     val result = SQLRunner.runStatement( statement, sqlc )
 
-    result.collect().foreach(println)
+    // Assert
+    assertDataFrameEquals( expectedDeduction, result )
+
   }
 
-  test("testing more scala stuffs") {
+  test("Deduction transformation mapping test, multiple lines each with multiple deductions") {
 
     // Arrange
     val sqlc = sqlContext
     sqlContext.sparkContext.setLogLevel( "WARN" )
 
     // Load test data into dataframes
-    val line = populateDataFrameWithLineTestData( "line_MultipleLines.csv", sqlc )
-    val layer = populateDataFrameWithLayerTestData( "layer_MultipleLines.csv", sqlc )
-    val layerDeduction = populateDataFrameWithLayerDeductionTestData( "layer_deduction_MultipleDeductionMonotonicSeqMultipleLines.csv", sqlc )
+    val line = populateDataFrameWithLineTestData( "line_MulitpleDeductionMonotonicSeqMultipleLines.csv", sqlc )
+    val layer = populateDataFrameWithLayerTestData( "layer_MulitpleDeductionMonotonicSeqMultipleLines.csv", sqlc )
+    val layerDeduction = populateDataFrameWithLayerDeductionTestData( "layer_deduction_MulitpleDeductionMonotonicSeqMultipleLines.csv", sqlc )
 
-    val layerDeductionJoinedWithLine: SchemaRDD = layerDeduction.join(line, "layer_id").repartition(col("line_id")).sortWithinPartitions(col("sequence_no"))
-
-    val expectedDeduction = populateDataFrameWithDeductionTestData( "deduction_MultipleDeductionMonotonicSeqMultipleLines.csv", sqlc )
-
-    val deductions = layerDeductionJoinedWithLine.mapPartitions( iterator => {
-      val list: List[Row] = iterator.toList
-      val newList = DeductionUtils.getStuff(list)
-      newList.iterator
-    })
+    val expectedDeduction = populateDataFrameWithDeductionTestData( "deduction_MulitpleDeductionMonotonicSeqMultipleLines.csv", sqlc )
 
     line.registerTempTable( "line" )
     layer.registerTempTable( "layer" )
     layerDeduction.registerTempTable( "layer_deduction" )
 
-    val deductionSchema = new StructType(Array(
-      StructField("line_id",IntegerType, false),
-      StructField("deduction_id",IntegerType, false),
-      StructField("sequence_no",IntegerType, false),
-      StructField("currentnet",StringType, false),
-      StructField("calculateddeductionamount",StringType, false)
-    ))
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( "Transformation/Deduction.hql" )
 
-    sqlc.createDataFrame(deductions,deductionSchema).registerTempTable("tmpTable")
-
-    val statement = utils.loadHQLStatementFromResource( "Transformation/DeductionTest.hql" )
+    // Act
     val result = SQLRunner.runStatement( statement, sqlc )
 
-    println(result.count())
-    assertDataFrameEquals(expectedDeduction,result)
+    // Assert
+    assertDataFrameEquals( expectedDeduction, result )
 
   }
-
-
-  def add100(value : Int) : Int = {
-    value+100
-  }
-
 }
