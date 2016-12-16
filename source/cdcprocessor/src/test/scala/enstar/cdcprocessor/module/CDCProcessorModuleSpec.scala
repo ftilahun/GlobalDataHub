8fad9f04c08bd1ba6c1912c68bd250a4929b6d90
Ciaran Kearney <C.Kearney@kainos.com>
2016-12-16 15:31:53 +0000

    feature/cdcprocessor: added module class

---
+++ source/cdcprocessor/src/test/scala/enstar/cdcprocessor/module/CDCProcessorModuleSpec.scala
@@ -0,0 +1,35 @@
+package enstar.cdcprocessor.module
+
+import enstar.cdcprocessor.io.{ DataFrameReader, DataFrameWriter }
+import enstar.cdcprocessor.properties.CDCProperties
+import enstar.cdcprocessor.udfs.UserFunctions
+import org.scalatest.{ FlatSpec, GivenWhenThen, Matchers }
+
+/**
+  * Unit tests for CDCLoaderModule
+  */
+class CDCProcessorModuleSpec extends FlatSpec with GivenWhenThen with Matchers {
+
+  "CDCLoaderModuleSpec" should "Return correct tyoes" in {
+    def typeCheck[T](value: T) = value match {
+
+      case _: DataFrameReader => "DataFrameReader"
+      case _: DataFrameWriter => "DataFrameWriter"
+      case _: UserFunctions   => "UserFunctions"
+      case _: CDCProperties   => "CDCProperties"
+    }
+
+    val argsArray = Array[String](
+      "--changeInput", "c",
+      "--activeOutput", "a",
+      "--idColumnName", "i",
+      "--transactionColumnName", "t",
+      "--changeSequenceColumnName", "c"
+    )
+
+    typeCheck(CDCProcessorModule.dataFrameReader) should be("DataFrameReader")
+    typeCheck(CDCProcessorModule.dataFrameWriter) should be("DataFrameWriter")
+    typeCheck(CDCProcessorModule.userFunctions) should be("UserFunctions")
+    typeCheck(CDCProcessorModule.properties(argsArray)) should be("CDCProperties")
+  }
+}
