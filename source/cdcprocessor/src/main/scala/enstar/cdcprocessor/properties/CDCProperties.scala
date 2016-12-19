73b3b6afc8254a515ab3c758b38d7bd0e09f1cf7
Ciaran Kearney <C.Kearney@kainos.com>
2016-12-19 16:36:33 +0000

    feature/cdccloserecords: added required properties to object

--- source/cdcprocessor/src/main/scala/enstar/cdcprocessor/properties/CDCProperties.scala
+++ source/cdcprocessor/src/main/scala/enstar/cdcprocessor/properties/CDCProperties.scala
@@ -5,8 +5,16 @@ package enstar.cdcprocessor.properties
  */
 case class CDCProperties(idColumnName: String = "",
                          transactionColumnName: String = "",
+                         transactionTimeStampColumnName: String = "",
+                         operationColumnName: String = "",
+                         operationColumnValueBefore: String = "B",
+                         operationColumnValueInsert: String = "I",
+                         operationColumnValueUpdate: String = "U",
+                         operationColumnValueDelete: String = "D",
                          changeSequenceColumnName: String = "",
                          changeInputDir: String = "",
                          activeOutput: String = "",
-                         attunityColumnPrefix: String = "")
+                         attunityColumnPrefix: String = "",
+                         validFromColumnName: String = "",
+                         validToColumnName: String = "")
     extends Serializable
