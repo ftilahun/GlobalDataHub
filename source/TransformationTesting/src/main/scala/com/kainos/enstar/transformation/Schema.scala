7ed48d41e4ea412e3781ded51dff50cbac490b3e
Neil Richardson <njrich28@gmail.com>
2016-12-19 10:24:28 +0000

    Some basic tests for Schema added

--- source/TransformationTesting/src/main/scala/com/kainos/enstar/transformation/Schema.scala
+++ source/TransformationTesting/src/main/scala/com/kainos/enstar/transformation/Schema.scala
@@ -8,17 +8,10 @@ import org.apache.spark.sql.types._
 class Schema( headers : Array[String] ) {
   import Schema._
 
-  val structType : StructType = {
-    val headerFields = headers.map {
-      case colDef( name, "string", _, _, nullable )              => new StructField( name, StringType, nullable != null )
-      case colDef( name, "long", _, _, nullable )                => new StructField( name, LongType, nullable != null )
-      case colDef( name, "int", _, _, nullable )                 => new StructField( name, IntegerType, nullable != null )
-      case colDef( name, "decimal", precision, scale, nullable ) => new StructField( name, DecimalType( precision.toInt, scale.toInt ), nullable != null )
-    }
-    new StructType( headerFields )
-  }
+  val structType : StructType = structFromHeaders( headers )
 
   def stringFieldsToAny( fields : Array[String] ) : Array[Any] = {
+    if (fields.length != structType.length) throw new IllegalArgumentException( s"Incorrect number of fields in row. Found ${fields.length}, expected ${structType.length}" )
     val fieldsWithType = fields zip structType.toList
     fieldsWithType.map {
       case ( field, fieldType ) =>
@@ -42,4 +35,18 @@ object Schema {
   val NULL = "[NULL]"
 
   def apply( headers : Array[String] ) : Schema = new Schema( headers )
+
+  private[transformation] def structFromHeaders( headers : Array[String] ) : StructType = {
+    try {
+      val headerFields = headers.map {
+        case colDef(name, "string", _, _, nullable) => new StructField(name, StringType, nullable != null)
+        case colDef(name, "long", _, _, nullable) => new StructField(name, LongType, nullable != null)
+        case colDef(name, "int", _, _, nullable) => new StructField(name, IntegerType, nullable != null)
+        case colDef(name, "decimal", precision, scale, nullable) => new StructField(name, DecimalType(precision.toInt, scale.toInt), nullable != null)
+      }
+      new StructType(headerFields)
+    } catch {
+      case me: MatchError => throw new IllegalArgumentException(s"Unable to parse column header ${me.getMessage()}")
+    }
+  }
 }
