37a392f5dce936e2733743a166747624ee7d9a00
Ciaran Kearney <C.Kearney@kainos.com>
2016-12-19 16:39:08 +0000

    feature/cdccloserecords: updated test data

--- source/cdcprocessor/src/test/scala/enstar/cdcprocessor/TestContexts.scala
+++ source/cdcprocessor/src/test/scala/enstar/cdcprocessor/TestContexts.scala
@@ -1,7 +1,8 @@
 package enstar.cdcprocessor
 
 import org.apache.spark.sql.functions.udf
-import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{ SparkConf, SparkContext }
 
 /**
@@ -16,10 +17,10 @@ object TestContexts {
       .setMaster("local[1]")
       .setAppName(this.getClass.getSimpleName))
   _sc.setLogLevel("OFF")
-  private val _sqlC: SQLContext = new SQLContext(_sc)
+  private val _sqlC: HiveContext = new HiveContext(_sc)
 
   def sparkContext: SparkContext = _sc
-  def sqlContext: SQLContext = _sqlC
+  def sqlContext: HiveContext = _sqlC
 
   def dummyData(numItems: Int): DataFrame = {
     val list = (1 to numItems).map { number =>
@@ -30,13 +31,14 @@ object TestContexts {
   }
 
   def changeDummyData(numItems: Int): DataFrame = {
-    val operations = List("INSERT", "UPDATE", "DELETE", "BEFOREIMAGE")
+    val operations = List("I", "U", "D", "B")
     val transactions = List("ONE", "TWO", "THREE", "FOUR")
     val changeOperation = udf(
       (num: Int) => operations(num % operations.length))
     val changeTransacion = udf(
       (num: Int) => transactions(num % operations.length)
     )
+    val toTime = udf(() => "9999-12-31 23:59:59.000")
     val dateTime = udf(() => "2016-07-12 11:12:32.111")
     val changeSeq = udf(
       (num: Int) => "20160712111232110000000000000000000" + num)
@@ -47,6 +49,7 @@ object TestContexts {
       .withColumn("header__changesequence", changeSeq(data("id")))
       .withColumn("header__transaction", changeTransacion(data("id")))
       .withColumn("header__id", data("id"))
+      .withColumn("validfrom", dateTime())
+      .withColumn("validto", toTime())
   }
-
 }
