b562250af847134feaadc2361e06b23d41ba159c
caoimheb <c.boyle@kainos.com>
2016-12-20 14:53:16 +0000

    feature/ASC_trustfund: Updated / added tests for ACS

--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/AnalysisCodeSplitTrustFund/ReconciliationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/AnalysisCodeSplitTrustFund/ReconciliationTests.scala
@@ -2,12 +2,74 @@ package com.kainos.enstar.test.TransformationUnitTesting.AnalysisCodeSplitTrustF
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import com.kainos.enstar.TransformationUnitTesting.{ SQLRunner, TransformationUnitTestingUtils }
-import org.apache.spark.sql.{ DataFrame, SQLContext }
 import org.scalatest.FunSuite
 
-/**
- * Created by caoimheb on 12/12/2016.
- */
 class ReconciliationTests extends FunSuite with DataFrameSuiteBase {
+
+  private val utils = new TransformationUnitTestingUtils
+  private val testDataInputPath = "/analysiscodesplit/trustfund/input/"
+  private val analysisCodeSplitTrustFundTransformation = "Transformation/AnalysisCodeSplitTrustFund.hql"
+
+  test( "Reconciliation on test case 1 input test data for AnalysisCodeSplit TrustFund" ) {
+
+    // Arrange //
+    // Use sqlContext from spark-testing-base
+    implicit val sqlc = sqlContext
+    sqlc.sparkContext.setLogLevel( "WARN" )
+
+    // Load test data into dataframe
+    val line = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_PrimaryTestData.csv" )
+    val layerTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_trust_fund_PrimaryTestData.csv" )
+    val lookupTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "lookup_trust_fund_PrimaryTestData.csv" )
+
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( analysisCodeSplitTrustFundTransformation )
+    val reconStatementInput = utils.loadHQLStatementFromResource( "Reconciliation/AnalysisCodeSplitTrustFund/InputRecordCount.hql" )
+    val reconStatementOutput = utils.loadHQLStatementFromResource( "Reconciliation/AnalysisCodeSplitTrustFund/OutputRecordCount.hql" )
+
+    // Act //
+    line.registerTempTable( "line" )
+    layerTrustFund.registerTempTable( "layer_trust_fund" )
+    lookupTrustFund.registerTempTable( "lookup_trust_fund" )
+    val output = SQLRunner.runStatement( statement, sqlc )
+    output.registerTempTable( "analysiscodesplit" )
+
+    val reconInput = SQLRunner.runStatement( reconStatementInput, sqlc )
+    val reconOutput = SQLRunner.runStatement( reconStatementOutput, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( reconInput, reconOutput )
+  }
+
+  test( "Reconciliation on test case 2 input test data for AnalysisCodeSplit TrustFund" ) {
+
+    // Arrange //
+    // Use sqlContext from spark-testing-base
+    implicit val sqlc = sqlContext
+    sqlc.sparkContext.setLogLevel( "WARN" )
+
+    // Load test data into dataframe
+    val line = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_ManyTrustFundToOneLayer.csv" )
+    val layerTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_trust_fund_ManyTrustFundToOneLayer.csv" )
+    val lookupTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "lookup_trust_fund_ManyTrustFundToOneLayer.csv" )
+
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( analysisCodeSplitTrustFundTransformation )
+    val reconStatementInput = utils.loadHQLStatementFromResource( "Reconciliation/AnalysisCodeSplitTrustFund/InputRecordCount.hql" )
+    val reconStatementOutput = utils.loadHQLStatementFromResource( "Reconciliation/AnalysisCodeSplitTrustFund/OutputRecordCount.hql" )
+
+    // Act //
+    line.registerTempTable( "line" )
+    layerTrustFund.registerTempTable( "layer_trust_fund" )
+    lookupTrustFund.registerTempTable( "lookup_trust_fund" )
+    val output = SQLRunner.runStatement( statement, sqlc )
+    output.registerTempTable( "analysiscodesplit" )
+
+    val reconInput = SQLRunner.runStatement( reconStatementInput, sqlc )
+    val reconOutput = SQLRunner.runStatement( reconStatementOutput, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( reconInput, reconOutput )
+  }
 
 }
--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/AnalysisCodeSplitTrustFund/TransformationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/AnalysisCodeSplitTrustFund/TransformationTests.scala
@@ -1,8 +1,67 @@
 package com.kainos.enstar.test.TransformationUnitTesting.AnalysisCodeSplitTrustFund
 
-/**
- * Created by caoimheb on 12/12/2016.
- */
-class TransformationTests {
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import com.kainos.enstar.TransformationUnitTesting.{ SQLRunner, TransformationUnitTestingUtils }
+import org.scalatest.FunSuite
+
+class TransformationTests extends FunSuite with DataFrameSuiteBase {
+
+  private val utils = new TransformationUnitTestingUtils
+  private val testDataInputPath = "/analysiscodesplit/trustfund/input/"
+  private val testDataOutputPath = "/analysiscodesplit/trustfund/output/"
+  private val analysisCodeSplitTrustFundTransformation = "Transformation/AnalysisCodeSplitTrustFund.hql"
+
+  test( "AnalysisCodeSplitTrustFundTransformation mapping test one line row to one layer_trust_fund row" ){
+
+    implicit val sqlc = sqlContext
+    sqlContext.sparkContext.setLogLevel( "WARN" )
+
+    // Arrange //
+    // Load test data into dataframe
+    val line = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_PrimaryTestData.csv" )
+    val layerTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_trust_fund_PrimaryTestData.csv" )
+    val lookupTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "lookup_trust_fund_PrimaryTestData.csv" )
+
+    // Load expected result into dataframe
+    val expectedAnalysisCodeSplit = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "analysiscodesplit_PrimaryTestData.csv" )
+
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( analysisCodeSplitTrustFundTransformation )
+
+    // Act //
+    line.registerTempTable( "line" )
+    layerTrustFund.registerTempTable( "layer_trust_fund" )
+    lookupTrustFund.registerTempTable( "lookup_trust_fund" )
+    val result = SQLRunner.runStatement( statement, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( expectedAnalysisCodeSplit.orderBy( "coveragereference" ), result.orderBy( "coveragereference" ) )
+  }
+
+  test( "AnalysisCodeSplitTrustFundTransformation mapping test many layer_trust_fund rows to one line row" ){
+
+    implicit val sqlc = sqlContext
+
+    // Arrange //
+    // Load test data into dataframe
+    val line = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_ManyTrustFundToOneLayer.csv" )
+    val layerTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_trust_fund_ManyTrustFundToOneLayer.csv" )
+    val lookupTrustFund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "lookup_trust_fund_ManyTrustFundToOneLayer.csv" )
+
+    // Load expected result into dataframe
+    val expectedAnalysisCodeSplit = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "analysiscodesplit_ManyTrustFundToOneLayer.csv" )
+
+    // Load the hql statement under test
+    val statement = utils.loadHQLStatementFromResource( analysisCodeSplitTrustFundTransformation )
+
+    // Act //
+    line.registerTempTable( "line" )
+    layerTrustFund.registerTempTable( "layer_trust_fund" )
+    lookupTrustFund.registerTempTable( "lookup_trust_fund" )
+    val result = SQLRunner.runStatement( statement, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( expectedAnalysisCodeSplit, result )
+  }
 
 }
