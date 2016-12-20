c2a74de68298b5fe5f4d1b8f3c97a35d50d50d0d
Neil Richardson <njrich28@gmail.com>
2016-12-20 12:17:00 +0000

    Created TransformationTestBase class to remove some of the boilerplate scala when creating transformation tests

--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Branch/TransformationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Branch/TransformationTests.scala
@@ -1,37 +1,15 @@
 package com.kainos.enstar.test.TransformationUnitTesting.Branch
 
-import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import com.kainos.enstar.TransformationUnitTesting.{ SQLRunner, TransformationUnitTestingUtils }
-import org.apache.spark.sql.DataFrame
-import org.scalatest.FunSuite
+import com.kainos.enstar.transformation.TransformationTestBase
 
 /**
  * Created by terences on 20/11/2016.
  */
-class TransformationTests extends FunSuite with DataFrameSuiteBase {
-
-  test( "BranchTransformation tes with Priamry data" ){
-
-    // Arrange //
-    // Use sqlContext from spark-testing-base
-    implicit val sqlc = sqlContext
-    sqlc.sparkContext.setLogLevel( "WARN" )
-    val utils = new TransformationUnitTestingUtils
-
-    // Load test data into dataframe
-    val lookupProfitCentre = utils.populateDataFrameFromCsvWithHeader( "/branch/input/lookup_profit_centre_PrimaryTestData.csv" )
-
-    // Load expected result into dataframe
-    val expectedBranch = utils.populateDataFrameFromCsvWithHeader( "/branch/output/branch_PriamryTestData.csv" )
-
-    // Load the hql statement under test
-    val statement = utils.loadHQLStatementFromResource( "Transformation/Branch.hql" )
-
-    // Act //
-    lookupProfitCentre.registerTempTable( "lookup_profit_centre" )
-    val result = SQLRunner.runStatement( statement, sqlc )
-
-    // Assert //
-    assertDataFrameEquals( expectedBranch, result )
-  }
-}
+class TransformationTests extends TransformationTestBase(
+  "BranchTransformation test with Primary data",
+  Map(
+    "lookup_profit_centre" -> "/branch/input/lookup_profit_centre_PrimaryTestData.csv"
+  ),
+  "/branch/output/branch_PriamryTestData.csv",
+  "Transformation/Branch.hql"
+)
