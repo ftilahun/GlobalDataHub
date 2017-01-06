39d6390fb65a3cb42b19a12e57160b7a199c05a0
Caoimhe Boyle <BoyleC@enstargroup.com>
2017-01-06 11:58:29 +0000

    Merged PR 106: Merge feature/amounts_bug_update to develop
     - feature/amounts_bug_update: Merged most up to date writtenpremium feed query
     - Merge branch 'develop' into feature/amounts_bug_update
     - feature/amounts_update: Updated calculations and tests based on reconciliation
     - Merge branch 'develop' into feature/amounts_bug_update

--- source/TransformationTesting/src/main/resources/Transformation/PolicyTransactionWrittenPremium.hql
+++ source/TransformationTesting/src/main/resources/Transformation/PolicyTransactionWrittenPremium.hql
@@ -9,13 +9,15 @@ SELECT
     false AS iscashtransactiontype,
     CAST(IF (line.business_type IN (1,17), 0,
         line.slip_income_amount * (line.reporting_line_pct / 100.00)  *
-        line_risk_code.risk_code_pct * layer_trust_fund.est_premium_split_pct) AS DECIMAL(18,6)) AS originalamount,
+        (line_risk_code.risk_code_pct / 100.00) *
+        (IF(layer_trust_fund.est_premium_split_pct IS NOT NULL, layer_trust_fund.est_premium_split_pct, CAST(100.00 AS DECIMAL(5,2))) / 100.00) ) AS DECIMAL(18,6)) AS originalamount,
     layer.premium_ccy AS originalcurrencycode,
     line.risk_reference AS policynumber,
     CAST(line.layer_id AS STRING) AS sectionreference,
     CAST(IF(line.business_type IN (1,17), 0,
         (line.slip_income_amount / layer.premium_roe )* (line.reporting_line_pct / 100.00)  *
-        line_risk_code.risk_code_pct * layer_trust_fund.est_premium_split_pct ) AS DECIMAL(18,6)) AS settlementamount,
+        (line_risk_code.risk_code_pct / 100.00) *
+        (IF(layer_trust_fund.est_premium_split_pct IS NOT NULL, layer_trust_fund.est_premium_split_pct, CAST(100.00 AS DECIMAL(5,2))) / 100.00) ) AS DECIMAL(18,6)) AS settlementamount,
     line.epi_settlement_ccy AS settlementcurrencycode,
     CAST(layer.inception_date AS STRING) AS transactiondate,
     "WrittenPremiumOurShare" AS transactiontypecode,
@@ -29,7 +31,7 @@ SELECT
 FROM
 
     line
-    JOIN layer
+    INNER JOIN layer
     ON layer.layer_id = line.layer_id
     LEFT JOIN line_risk_code
     ON line.line_id = line_risk_code.line_id
--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/PolicyTransactionWrittenPremium/TransformationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/PolicyTransactionWrittenPremium/TransformationTests.scala
@@ -1,4 +1,4 @@
-package com.kainos.enstar.test.TransformationUnitTesting.PolicyTransaction
+package com.kainos.enstar.test.TransformationUnitTesting.PolicyTransactionWrittenPremium
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import com.kainos.enstar.TransformationUnitTesting.{ SQLRunner, TransformationUnitTestingUtils }
@@ -26,7 +26,7 @@ class TransformationTests extends FunSuite with DataFrameSuiteBase {
     // in the transform due to the LEFT join on line_risk_code and layer_trust_fund tables.
     val expectedPolicyTransaction = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "policytransaction_PrimaryTestData.csv" )
 
-    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransaction_WrittenPremium.hql" )
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransactionWrittenPremium.hql" )
 
     // Act
     line.registerTempTable( "line" )
@@ -55,7 +55,7 @@ class TransformationTests extends FunSuite with DataFrameSuiteBase {
     // in the transform due to the LEFT join on line_risk_code and layer_trust_fund tables.
     val expectedPolicyTransaction = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "policytransaction_VariousNullValues.csv" )
 
-    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransaction_WrittenPremium.hql" )
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransactionWrittenPremium.hql" )
 
     // Act //
     line.registerTempTable( "line" )
@@ -85,7 +85,7 @@ class TransformationTests extends FunSuite with DataFrameSuiteBase {
     // in the transform due to the LEFT join on line_risk_code and layer_trust_fund tables.
     val expectedPolicyTransaction = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "policytransaction_AmountAndSettlementCalculation.csv" )
 
-    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransaction_WrittenPremium.hql" )
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransactionWrittenPremium.hql" )
 
     // Act //
     line.registerTempTable( "line" )
@@ -115,7 +115,38 @@ class TransformationTests extends FunSuite with DataFrameSuiteBase {
     // in the transform due to the LEFT join on line_risk_code and layer_trust_fund tables.
     val expectedPolicyTransaction = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "policytransaction_PrimaryKeyMissingFields.csv" )
 
-    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransaction_WrittenPremium.hql" )
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransactionWrittenPremium.hql" )
+
+    // Act //
+    line.registerTempTable( "line" )
+    layer.registerTempTable( "layer" )
+    line_risk_code.registerTempTable( "line_risk_code" )
+    layer_trust_fund.registerTempTable( "layer_trust_fund" )
+
+    val result = SQLRunner.runStatement( hqlStatement, sqlc )
+
+    // Assert //
+    assertDataFrameEquals( expectedPolicyTransaction, result )
+
+  }
+
+  test( "PolicyTransaction_WP_Transformation testing the settlementamount and originalamount calculations when layer_trust_fund is null" ){
+
+    // Arrange //
+    implicit val sqlc = sqlContext
+    sqlContext.sparkContext.setLogLevel( "WARN" )
+
+    val line = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_BusinessTypeOutsideRange.csv" )
+    val layer = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_PrimaryTestData.csv" )
+    val line_risk_code = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "line_risk_code_PrimaryTestData.csv" )
+    val layer_trust_fund = utils.populateDataFrameFromCsvWithHeader( testDataInputPath + "layer_trust_fund_LayerIdNotInLine.csv" )
+
+    // The values for settlementamount and originalamount have been calculated using the ELSE value of 100 for
+    // layer_trust_fund.est_premium_split_pct as this value comes through as null in this case due to the LEFT JOIN to
+    // the layer_trust_fund table
+    val expectedPolicyTransaction = utils.populateDataFrameFromCsvWithHeader( testDataOutputPath + "policytransaction_CalculationsWithNullLayerTrustFund.csv" )
+
+    val hqlStatement = utils.loadHQLStatementFromResource( "Transformation/PolicyTransactionWrittenPremium.hql" )
 
     // Act //
     line.registerTempTable( "line" )
