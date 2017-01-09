665c36765604f315c0990cc3200f9786df0c3f2f
Síona Murray <s.murray@kainos.com>
2017-01-09 10:00:11 +0000

    feature/insured : Applying review comments.

--- source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Insured/TransformationTests.scala
+++ source/TransformationTesting/src/test/scala/com/kainos/enstar/test/TransformationUnitTesting/Insured/TransformationTests.scala
@@ -6,22 +6,26 @@ import org.scalatest.FunSuite
 
 class TransformationTests extends FunSuite with DataFrameSuiteBase {
 
-  test( "InsuredTransformation tes with Primary data" ){
+  private val utils = new TransformationUnitTestingUtils
+  private val testDataInputDirPath = "/insured/input/"
+  private val testDataOutputDirPath = "/insured/output/"
+  private val insuredTransformationPath = "Transformation/Insured.hql"
+
+  test( "InsuredTransformation test with Primary data" ){
 
     // Arrange //
     // Use sqlContext from spark-testing-base
     implicit val sqlc = sqlContext
     sqlc.sparkContext.setLogLevel( "WARN" )
-    val utils = new TransformationUnitTestingUtils
 
     // Load test data into dataframe
-    val organisation = utils.populateDataFrameFromCsvWithHeader( "/insured/input/organisation_PrimaryTestData.csv" )
+    val organisation = utils.populateDataFrameFromCsvWithHeader( testDataInputDirPath + "organisation_PrimaryTestData.csv" )
 
     // Load expected result into dataframe
-    val expectedInsured = utils.populateDataFrameFromCsvWithHeader( "/insured/output/insured_PrimaryTestData.csv" )
+    val expectedInsured = utils.populateDataFrameFromCsvWithHeader( testDataOutputDirPath + "insured_PrimaryTestData.csv" )
 
     // Load the hql statement under test
-    val statement = utils.loadHQLStatementFromResource( "Transformation/Insured.hql" )
+    val statement = utils.loadHQLStatementFromResource( insuredTransformationPath )
 
     // Act //
     organisation.registerTempTable( "organisation" )
