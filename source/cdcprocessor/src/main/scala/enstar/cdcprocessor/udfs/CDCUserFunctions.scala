998ca6f6c1c541a04f032f5149a844efed7a6888
Ciaran Kearney <C.Kearney@kainos.com>
2017-01-10 10:39:50 +0000

    feature/cdctimewindow: added functions for timewindowfilter, active record filter, joining dataframes

--- source/cdcprocessor/src/main/scala/enstar/cdcprocessor/udfs/CDCUserFunctions.scala
+++ source/cdcprocessor/src/main/scala/enstar/cdcprocessor/udfs/CDCUserFunctions.scala
@@ -25,10 +25,10 @@ class CDCUserFunctions extends UserFunctions {
     DateTimeFormat.forPattern(format).print(new DateTime())
 
   /**
-   * Group changes in a dataframe by transaction
-   * @param df the dataframe to group
+   * Group changes in a DataFrame by transaction
+   * @param df the DataFrame to group
    * @param properties the GDH properties object
-   * @return a dataframe filtered by group
+   * @return a DataFrame filtered by group
    */
   def groupByTransactionAndKey(df: DataFrame,
                                properties: CDCProperties): DataFrame = {
@@ -59,10 +59,10 @@ class CDCUserFunctions extends UserFunctions {
   }
 
   /**
-   * Drops attunity columns from the output dataset
-   * @param df the dataframe to operate on
+   * Drops attunity columns from the output data set
+   * @param df the DataFrame to operate on
    * @param properties the properties object
-   * @return a dataframe
+   * @return a DataFrame
    */
   def dropAttunityColumns(df: DataFrame,
                           properties: CDCProperties): DataFrame =
@@ -72,17 +72,17 @@ class CDCUserFunctions extends UserFunctions {
         .map(col): _*)
 
   /**
-   * Sort a dataframe by the business key
-   * @param dataFrame the dataframe to sort
+   * Sort a DataFrame by the business key
+   * @param dataFrame the DataFrame to sort
    * @param properties the properties object
-   * @return a dataframe
+   * @return a DataFrame
    */
   def sortByKey(dataFrame: DataFrame, properties: CDCProperties): DataFrame =
     dataFrame.sort(asc(properties.idColumnName))
 
   /**
-   * Filter before image records from the dataframe
-   * @param dataFrame the dataframe to filter
+   * Filter before image records from the DataFrame
+   * @param dataFrame the DataFrame to filter
    * @param properties the properties object
    * @return
    */
@@ -116,10 +116,10 @@ class CDCUserFunctions extends UserFunctions {
   }
 
   /**
-   * Add an 'active' column to the passed in dataframe based on the value of the transaction timestamp
-   * @param dataFrame the dataframe to add the column to
+   * Add an 'active' column to the passed in DataFrame based on the value of the transaction timestamp
+   * @param dataFrame the DataFrame to add the column to
    * @param properties the properties object
-   * @return a dataframe
+   * @return a DataFrame
    */
   def addActiveColumn(dataFrame: DataFrame,
                       properties: CDCProperties): DataFrame = {
@@ -131,20 +131,20 @@ class CDCUserFunctions extends UserFunctions {
   }
 
   /**
-   * drop the active column from the passed in dataframe
-   * @param dataFrame the dataframe to operate on
+   * drop the active column from the passed in DataFrame
+   * @param dataFrame the DataFrame to operate on
    * @param properties the properties object
-   * @return a dataframe
+   * @return a DataFrame
    */
   def dropActiveColumn(dataFrame: DataFrame,
                        properties: CDCProperties): DataFrame =
     dataFrame.drop(properties.activeColumnName)
 
   /**
-   * Close records that have been superseded in the dataframe
-   * @param dataFrame the dataframe to process
+   * Close records that have been superseded in the DataFrame
+   * @param dataFrame the DataFrame to process
    * @param properties the properties object
-   * @return a dataframe
+   * @return a DataFrame
    */
   def closeRecords(dataFrame: DataFrame,
                    properties: CDCProperties): DataFrame = {
@@ -167,21 +167,50 @@ class CDCUserFunctions extends UserFunctions {
   }
 
   /**
-    * Filter a dataframe based on a time window, this method filters the passed in DF based on the
-    * value in the transaction timestamp column.
-    * @param dataFrame the dataframe to filter
-    * @param properties the properties object
-    * @param returnMature set to true to only return data that is older than the time window
-    * @return a dataframe
-    */
-  def filterOnTimeWindow(dataFrame: DataFrame, properties: CDCProperties, returnMature: Boolean = true): DataFrame = {
+   * Filter a DataFrame based on a time window, this method filters the passed in DF based on the
+   * value in the transaction timestamp column.
+   * @param dataFrame the DataFrame to filter
+   * @param properties the properties object
+   * @param returnMature set to true to only return data that is older than the time window
+   * @return a DataFrame
+   */
+  def filterOnTimeWindow(dataFrame: DataFrame,
+                         properties: CDCProperties,
+                         returnMature: Boolean = true): DataFrame = {
     val filterBeforeTimeWindow = udf(
       (timeStampString: String) => {
-        val currentTime = new time.DateTime().minusHours(properties.timeWindowInHours)
-        val timeStamp = DateTimeFormat.forPattern(properties.attunityDateFormat).parseDateTime(timeStampString)
+        val currentTime =
+          new time.DateTime().minusHours(properties.timeWindowInHours)
+        val timeStamp = DateTimeFormat
+          .forPattern(properties.attunityDateFormat)
+          .parseDateTime(timeStampString)
         timeStamp.isBefore(currentTime) == returnMature
       }
     )
-    dataFrame.filter(filterBeforeTimeWindow(dataFrame(properties.transactionTimeStampColumnName)))
+    dataFrame.filter(
+      filterBeforeTimeWindow(
+        dataFrame(properties.transactionTimeStampColumnName)))
   }
+
+  /**
+   * Filter a DataFrame based on the value of the 'active' column
+   * @param allRecords the DataFrame to filter
+   * @param properties the properties object
+   * @param returnActive set to false to return records where active=false
+   * @return a DataFrame
+   */
+  def filterOnActive(allRecords: DataFrame,
+                     properties: CDCProperties,
+                     returnActive: Boolean = true): DataFrame =
+    allRecords.filter(allRecords(properties.activeColumnName) === returnActive)
+
+  /**
+   * Join two DataFrames
+   *
+   * @param df1 the DataFrame to join to
+   * @param df2 the DataFrame to Join
+   * @return a DataFrame
+   */
+  override def unionAll(df1: DataFrame, df2: DataFrame): DataFrame =
+    df1.unionAll(df2)
 }
