71150d2581931059bc3d9ab0e973c84b42c50772
Ciaran Kearney <C.Kearney@kainos.com>
2017-01-18 11:13:28 +0000

    feature/CDCPropertiesPerTable: updated failing test

--- source/propertyparsers/src/main/scala/enstar/cdctableprocessor/properties/CDCProperties.scala
+++ source/propertyparsers/src/main/scala/enstar/cdctableprocessor/properties/CDCProperties.scala
@@ -1,40 +1,40 @@
 package enstar.cdctableprocessor.properties
 
 /**
-  * Properties class
-  */
+ * Properties class
+ */
 case class CDCProperties(
-                         //common properties for all tables
-                         idColumnName: String = "",
-                         transactionTimeStampColumnName: String = "",
-                         operationColumnName: String = "",
-                         operationColumnValueBefore: String = "B",
-                         operationColumnValueInsert: String = "I",
-                         operationColumnValueUpdate: String = "U",
-                         operationColumnValueDelete: String = "D",
-                         transactionIdColumnName: String = "",
-                         changeSequenceColumnName: String = "",
-                         attunityColumnPrefix: String = "",
-                         validFromColumnName: String = "",
-                         validToColumnName: String = "",
-                         activeColumnName: String = "",
-                         attunityCutoff: String = "",
-                         attunityDateFormat: String = "",
-                         attunityDateFormatShort: String = "",
-                         //per table properties
-                         changeInputDir: String = "",
-                         activeInput: String = "",
-                         activeOutput: String = "",
-                         immatureChangesOutput: String = "",
-                         historyOutput: String = "",
-                         metricsOutputDir: Option[String] = None)
+  //common properties for all tables
+  idColumnName: String = "",
+  transactionTimeStampColumnName: String = "",
+  operationColumnName: String = "",
+  operationColumnValueBefore: String = "B",
+  operationColumnValueInsert: String = "I",
+  operationColumnValueUpdate: String = "U",
+  operationColumnValueDelete: String = "D",
+  transactionIdColumnName: String = "",
+  changeSequenceColumnName: String = "",
+  attunityColumnPrefix: String = "",
+  validFromColumnName: String = "",
+  validToColumnName: String = "",
+  activeColumnName: String = "",
+  attunityCutoff: String = "",
+  attunityDateFormat: String = "",
+  attunityDateFormatShort: String = "",
+  //per table properties
+  changeInputDir: String = "",
+  activeInput: String = "",
+  activeOutput: String = "",
+  immatureChangesOutput: String = "",
+  historyOutput: String = "",
+  metricsOutputDir: Option[String] = None)
     extends Serializable
     with PropertiesToMap {
 
   /**
-    * Covert the properties object to a Map of String -> String
-    * @return a map of property values
-    */
+   * Covert the properties object to a Map of String -> String
+   * @return a map of property values
+   */
   def toMap: Map[String, String] = {
     val optionalArgs: Map[String, String] = if (metricsOutputDir.isDefined) {
       Map[String, String]("--metricsOutputDir" -> metricsOutputDir.get)
@@ -67,22 +67,22 @@ case class CDCProperties(
   }
 
   /**
-    * Create a new CDCProperties object with the supplied input/output directories
-    *
-    * @param changeInputDir The directory to read new changes from
-    * @param activeInput The directory to read the previously active records from
-    * @param activeOutput the diretory to write active records to
-    * @param immatureChangesOutput the directory to write rejected changes to
-    * @param historyOutput the directory to write closed records to
-    * @param metricsOutputDir the optional directory to write metrics to
-    * @return
-    */
+   * Create a new CDCProperties object with the supplied input/output directories
+   *
+   * @param changeInputDir The directory to read new changes from
+   * @param activeInput The directory to read the previously active records from
+   * @param activeOutput the diretory to write active records to
+   * @param immatureChangesOutput the directory to write rejected changes to
+   * @param historyOutput the directory to write closed records to
+   * @param metricsOutputDir the optional directory to write metrics to
+   * @return
+   */
   def updateDirectories(changeInputDir: String,
-                 activeInput: String,
-                 activeOutput: String,
-                 immatureChangesOutput: String,
-                 historyOutput: String,
-                 metricsOutputDir: Option[String]): CDCProperties = {
+                        activeInput: String,
+                        activeOutput: String,
+                        immatureChangesOutput: String,
+                        historyOutput: String,
+                        metricsOutputDir: Option[String]): CDCProperties = {
     this.copy(
       changeInputDir = changeInputDir,
       activeInput = activeInput,
